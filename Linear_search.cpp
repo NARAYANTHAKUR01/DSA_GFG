0adb347d8847bb23ddfa57f9713756b40e60a1fe
NARYANTHAKUR01 <narayansinghrathaur01@gmail.com>
2026-04-20 17:20:36 +0530

    DAY1

---
+++ Linear_search.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void linear_search(int *arr,int n,int key){
+for(int i = 0;i<n;i++){
+    if(*(arr+i) == key){
+        cout<<"found at "<<i+1;
+        return;
+    }
+}
+cout<<"not found";
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    linear_search(arr,10,2);
+    return 0;
+}
+// #include<iostream>
+// using namespace std;
+
+// void linear_search(int arr[],int n,int key){
+// for(int i = 0;i<n;i++){
+//     if(arr[i] == key){
+//         cout<<"found at "<<i+1;
+//         return;
+//     }
+// }
+// cout<<"not found";
+// }
+// int main()
+// {
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//     linear_search(arr,10,2);
+//     return 0;
+// }
